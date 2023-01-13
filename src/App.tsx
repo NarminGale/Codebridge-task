836318aadc37d0b6fc8991f4ed899331bc5257be
Narmin Aliyeva <narmingale@gmail.com>
2023-01-14 02:05:45 +0400

    fix: homepage ui style

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Container from '@mui/material/Container'
+import Box from '@mui/material/Box'
+import Grid from '@mui/material/Grid'
+import PostCard from './components/PostCard/PostCard'
+import { useEffect, useState } from 'react'
+import {
+  Divider,
+  FormControl,
+  InputAdornment,
+  OutlinedInput,
+} from '@mui/material'
+import SearchIcon from '@mui/icons-material/Search'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Article {
+  imageUrl: string
+  title: string
+  summary: string
+  publishedAt: string
 }
 
-export default App;
+export default function App() {
+  const [currentTutorial, setCurrentTutorial] = useState([])
+
+  useEffect(() => {
+    fetch('https://api.spaceflightnewsapi.net/v3/articles')
+      .then(response => response.json())
+      .then(data => setCurrentTutorial(data))
+  }, [])
+
+  return (
+    <Container maxWidth='lg' className='homepage' sx={{ my: 6 }}>
+      <header className='homepage-header'>Filter by keywords</header>
+      <FormControl sx={{ my: 2, width: 600 }}>
+        <OutlinedInput
+          sx={{ height: '40px', fontSize: '16px' }}
+          placeholder='The most successful IT companies in 2020'
+          startAdornment={
+            <InputAdornment position='start'>
+              <SearchIcon sx={{ fontSize: '20px' }} />
+            </InputAdornment>
+          }
+        />
+      </FormControl>
+      <main className='homepage-content'>
+        Results: {currentTutorial.length}
+        <Divider />
+        <Box sx={{ flexGrow: 1, marginY: '30px' }}>
+          <Grid
+            container
+            spacing={{ xs: 2, md: 3, lg: 4 }}
+            columns={{ xs: 4, sm: 8, md: 12 }}
+          >
+            {currentTutorial.map((article: Article, index) => (
+              <Grid item xs={2} sm={4} md={4} key={index}>
+                <PostCard
+                  imageUrl={article.imageUrl}
+                  title={article.title}
+                  summary={article.summary}
+                  publishedAt={article.publishedAt}
+                />
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
+      </main>
+    </Container>
+  )
+}
---
+++ src/components/PostCard/PostCard.tsx
@@ -0,0 +1,54 @@
+import './PostCard.scss'
+
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
+import Typography from '@mui/material/Typography'
+import { Button, CardActionArea, CardActions } from '@mui/material'
+import CalendarTodayOutlinedIcon from '@mui/icons-material/CalendarTodayOutlined'
+export default function PostCard(props: any) {
+  // ts date format
+  const date = new Date(props.publishedAt)
+  const dateFormatted = date.toDateString().split(' ').slice(1)
+
+  console.log(dateFormatted, 'dateFormatted')
+
+  return (
+    <Card sx={{ maxWidth: 400, height: 530 }}>
+      <CardActionArea>
+        <CardMedia
+          component='img'
+          height='217'
+          image={props.imageUrl}
+          alt='green iguana'
+        />
+        <CardContent>
+          <Typography>
+            <CalendarTodayOutlinedIcon fontSize={'small'} />
+            {`${dateFormatted[0]} ${dateFormatted[1]}th, ${dateFormatted[2]}`}
+          </Typography>
+          <Typography
+            className='article-title'
+            gutterBottom
+            variant='h5'
+            component='div'
+          >
+            {props.title}
+          </Typography>
+          <Typography
+            className='article-content'
+            variant='body2'
+            color='text.secondary'
+          >
+            {props.summary}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size='small' color='primary'>
+          read more ...
+        </Button>
+      </CardActions>
+    </Card>
+  )
+}
